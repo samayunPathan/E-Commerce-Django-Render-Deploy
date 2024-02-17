7a833a4c7ceb79e0de9a4e72bdcba462c01dadee
samayun <samayunp@gmail.com>
2024-02-17 11:19:10 +0600

    fixed11

--- My_Ecom_Project/settings.py
+++ My_Ecom_Project/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 # ALLOWED_HOSTS = ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["*"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
